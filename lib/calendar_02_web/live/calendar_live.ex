ae7e72ce72ccbdcd97b79d5d8fed2bcbe8ebe01a
Dang Nguyen Hai <dangnh@nhg.vn>
2020-08-31 13:49:15 +0700

    calendar basic

--- lib/calendar_02_web/live/calendar_live.ex
+++ lib/calendar_02_web/live/calendar_live.ex
@@ -4,68 +4,41 @@ defmodule Calendar02Web.CalendarLive do
   use Calendar02Web, :live_view
   use Timex
 
-  def render(assigns) do
-    ~L"""
-    <div>
-    <%= live_component(
-          @socket,
-          Calendar02Web.CalendarLiveComponent,
-          title: "Assigns Title"
-        )
-    %>
-    </div>
-    """
-  end
+  @week_start_at :mon
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    current_date = Timex.now
+
+    assigns = [
+      conn: socket,
+      current_date: current_date,
+      day_names: day_names(@week_start_at),
+      week_rows: week_rows(current_date)
+    ]
+
+    {:ok, assign(socket, assigns)}
   end
 
-  def handle_params(_params, _uri, socket) do
-    {:noreply, socket}
+  def render(assigns) do
+    Calendar02Web.CalendarView.render("index.html", assigns)
   end
 
-  # @week_start_at :mon
+  defp day_names(:sun), do:  [7, 1, 2, 3, 4, 5, 6] |> Enum.map(&Timex.day_shortname/1)
+  defp day_names(_), do:  [1, 2, 3, 4, 5, 6, 7] |> Enum.map(&Timex.day_shortname/1)
 
-  # def mount(_session, socket) do
-  #   current_date = Timex.now
+  defp week_rows(current_date) do
+    first =
+      current_date
+      |> Timex.beginning_of_month()
+      |> Timex.beginning_of_week(@week_start_at)
 
-  #   assigns = [
-  #     conn: socket,
-  #     current_date: current_date,
-  #     day_names: day_names(@week_start_at),
-  #     week_rows: week_rows(current_date)
-  #   ]
+    last =
+      current_date
+      |> Timex.end_of_month()
+      |> Timex.end_of_week(@week_start_at)
 
-  #   {:ok, assign(socket, assigns)}
-  # end
-
-  # # def render(assigns) do
-  # #   Calendar02Web.CalendarView.render("index.html", assigns)
-  # # end
-
-  # def render(assigns) do
-  #   ~L"""
-  #   <h1>Title</h1>
-  #   """
-  # end
-
-  # defp day_names(:sun), do:  [7, 1, 2, 3, 4, 5, 6] |> Enum.map(&Timex.day_shortname/1)
-  # defp day_names(_), do:  [1, 2, 3, 4, 5, 6, 7] |> Enum.map(&Timex.day_shortname/1)
-
-  # defp week_rows(current_date) do
-  #   first =
-  #     current_date
-  #     |> Timex.beginning_of_month()
-  #     |> Timex.beginning_of_week(@week_start_at)
-
-  #   last =
-  #     current_date
-  #     |> Timex.end_of_month()
-  #     |> Timex.end_of_week(@week_start_at)
-
-  #   Interval.new(from: first, until: last)
-  #   |> Enum.map(& &1)
-  #   |> Enum.chunk_every(7)
-  # end
+    Interval.new(from: first, until: last)
+    |> Enum.map(& &1)
+    |> Enum.chunk_every(7)
+  end
 end
--- lib/calendar_02_web/live/calendar_live_component.ex
+++ lib/calendar_02_web/live/calendar_live_component.ex
@@ -1,59 +1,54 @@
 defmodule Calendar02Web.CalendarLiveComponent do
   use Phoenix.LiveComponent
 
-  def render(assigns) do
-    Calendar02Web.CalendarView.render("index.html", assigns)
-    # ~L"""
-    # <h1><%= @title %></h1>
-    # """
-  end
+  # def render(assigns) do
+  #   Calendar02Web.CalendarView.render("index.html", assigns)
+  # end
 
-  @spec mount(any) :: {:ok, any}
-  def mount(socket) do
-    {:ok, socket}
-  end
+  # @spec mount(any) :: {:ok, any}
+  # def mount(socket) do
+  #   {:ok, socket}
+  # end
 
-  def update(%{title: title} = _assigns, socket) do
-    current_date = Timex.now
-    day_names = day_names(@week_start_at)
-    week_rows = week_rows(current_date)
+  # def update(%{title: title} = _assigns, socket) do
+  #   current_date = Timex.now
+  #   day_names = day_names(@week_start_at)
+  #   week_rows = week_rows(current_date)
 
+  #   {:ok, assign(socket, title: title, current_date: current_date, day_names: day_names, week_rows: week_rows)}
+  # end
 
+  # @week_start_at :mon
 
-    {:ok, assign(socket, title: title, current_date: current_date, day_names: day_names, week_rows: week_rows)}
-  end
+  # def mount(_session, socket) do
+  #   current_date = Timex.now
 
-  @week_start_at :mon
+  #   assigns = [
+  #     conn: socket,
+  #     current_date: current_date,
+  #     day_names: day_names(@week_start_at),
+  #     week_rows: week_rows(current_date)
+  #   ]
 
-  def mount(_session, socket) do
-    current_date = Timex.now
+  #   {:ok, assign(socket, assigns)}
+  # end
 
-    assigns = [
-      conn: socket,
-      current_date: current_date,
-      day_names: day_names(@week_start_at),
-      week_rows: week_rows(current_date)
-    ]
+  # defp day_names(:sun), do:  [7, 1, 2, 3, 4, 5, 6] |> Enum.map(&Timex.day_shortname/1)
+  # defp day_names(_), do:  [1, 2, 3, 4, 5, 6, 7] |> Enum.map(&Timex.day_shortname/1)
 
-    {:ok, assign(socket, assigns)}
-  end
+  # defp week_rows(current_date) do
+  #   first =
+  #     current_date
+  #     |> Timex.beginning_of_month()
+  #     |> Timex.beginning_of_week(@week_start_at)
 
-  defp day_names(:sun), do:  [7, 1, 2, 3, 4, 5, 6] |> Enum.map(&Timex.day_shortname/1)
-  defp day_names(_), do:  [1, 2, 3, 4, 5, 6, 7] |> Enum.map(&Timex.day_shortname/1)
+  #   last =
+  #     current_date
+  #     |> Timex.end_of_month()
+  #     |> Timex.end_of_week(@week_start_at)
 
-  defp week_rows(current_date) do
-    first =
-      current_date
-      |> Timex.beginning_of_month()
-      |> Timex.beginning_of_week(@week_start_at)
-
-    last =
-      current_date
-      |> Timex.end_of_month()
-      |> Timex.end_of_week(@week_start_at)
-
-      Timex.Interval.new(from: first, until: last)
-    |> Enum.map(& &1)
-    |> Enum.chunk_every(7)
-  end
+  #     Timex.Interval.new(from: first, until: last)
+  #   |> Enum.map(& &1)
+  #   |> Enum.chunk_every(7)
+  # end
 end
