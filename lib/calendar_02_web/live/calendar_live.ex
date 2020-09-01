2b65a6ba5c8f712e5ff5e85c0fb6ef277d3e04bd
Dang Nguyen Hai <dangnh@nhg.vn>
2020-09-01 11:42:31 +0700

    full view calendar

--- lib/calendar_02_web/live/calendar_live.ex
+++ lib/calendar_02_web/live/calendar_live.ex
@@ -2,12 +2,14 @@ defmodule Calendar02Web.CalendarLive do
   #use Phoenix.LiveView
   #use Phoenix.LiveComponent
   use Calendar02Web, :live_view
+  use Calendar02Web, :controller
   use Timex
 
   @week_start_at :mon
 
   def mount(_params, _session, socket) do
     current_date = Timex.now
+    #assigns_day = Map.put(assigns, :day_class, day_class(assigns))
 
     assigns = [
       conn: socket,
@@ -40,5 +42,41 @@ defmodule Calendar02Web.CalendarLive do
     Interval.new(from: first, until: last)
     |> Enum.map(& &1)
     |> Enum.chunk_every(7)
+  end
+
+  def handle_event("prev-month", _, socket) do
+    current_date = Timex.shift(socket.assigns.current_date, months: -1)
+
+    assigns = [
+      current_date: current_date,
+      week_rows: week_rows(current_date)
+    ]
+
+    {:noreply, assign(socket, assigns)}
+  end
+
+  def handle_event("next-month", _, socket) do
+    current_date = Timex.shift(socket.assigns.current_date, months: 1)
+
+    assigns = [
+      current_date: current_date,
+      week_rows: week_rows(current_date)
+    ]
+
+    {:noreply, assign(socket, assigns)}
+  end
+
+  def handle_event("pick-date", %{"date" => date}, socket) do
+    current_date = Timex.parse!(date, "{YYYY}-{0M}-{D}")
+
+    assigns = [
+      current_date: current_date
+    ]
+
+    IO.inspect date
+    #Routes.page_path(@conn, :index)
+    #Routes.page_path(:index)
+    #Calendar02Web.Router.page_path(Calendar02Web.Endpoint, :index)
+    {:noreply, assign(socket, assigns)}
   end
 end
--- lib/calendar_02_web/live/calendar_live_component.ex
+++ lib/calendar_02_web/live/calendar_live_component.ex
@@ -1,54 +1,47 @@
 defmodule Calendar02Web.CalendarLiveComponent do
   use Phoenix.LiveComponent
+  use Timex
 
-  # def render(assigns) do
-  #   Calendar02Web.CalendarView.render("index.html", assigns)
+  def render(assigns) do
+    assigns = Map.put(assigns, :day_class, day_class(assigns))
+
+    ~L"""
+    <td phx-click="pick-date" phx-value-date="<%= Timex.format!(@day, "%Y-%m-%d", :strftime) %>" class="<%= @day_class %>">
+      <%= Timex.format!(@day, "%d", :strftime) %>
+    </td>
+    """
+  end
+
+  defp day_class(assigns) do
+    cond do
+      current_date?(assigns) ->
+        "text-center text-2xl p-6 text-gray-600 border border-gray-200 bg-green-200 cursor-pointer"
+      today?(assigns) ->
+        "text-center text-2xl p-6 text-gray-600 border border-gray-200 bg-blue-100 bg-green-300:hover cursor-pointer"
+      # weekend?(assigns) ->
+      #   "text-center text-2xl p-6 text-gray-400 border border-gray-200 border-red-500 cursor-not-allowed"
+      other_month?(assigns) ->
+        "text-center text-2xl p-6 text-gray-400 border border-gray-200 bg-gray-200 cursor-not-allowed"
+      true ->
+        "text-center text-2xl p-6 text-gray-600 border border-gray-200 bg-white bg-blue-100:hover cursor-pointer"
+    end
+  end
+
+  defp current_date?(assigns) do
+    Map.take(assigns.day, [:year, :month, :day]) == Map.take(assigns.current_date, [:year, :month, :day])
+    #IO.inspect assigns.current_date
+  end
+
+  defp today?(assigns) do
+    Map.take(assigns.day, [:year, :month, :day]) == Map.take(Timex.now, [:year, :month, :day])
+  end
+
+  # defp weekend?(assigns) do
+  #   (Timex.days_to_beginning_of_week(assigns.day) == 5 || Timex.days_to_beginning_of_week(assigns.day) == 6)
+  #     && Map.take(assigns.day, [:year, :month]) == Map.take(assigns.current_date, [:year, :month])
   # end
 
-  # @spec mount(any) :: {:ok, any}
-  # def mount(socket) do
-  #   {:ok, socket}
-  # end
-
-  # def update(%{title: title} = _assigns, socket) do
-  #   current_date = Timex.now
-  #   day_names = day_names(@week_start_at)
-  #   week_rows = week_rows(current_date)
-
-  #   {:ok, assign(socket, title: title, current_date: current_date, day_names: day_names, week_rows: week_rows)}
-  # end
-
-  # @week_start_at :mon
-
-  # def mount(_session, socket) do
-  #   current_date = Timex.now
-
-  #   assigns = [
-  #     conn: socket,
-  #     current_date: current_date,
-  #     day_names: day_names(@week_start_at),
-  #     week_rows: week_rows(current_date)
-  #   ]
-
-  #   {:ok, assign(socket, assigns)}
-  # end
-
-  # defp day_names(:sun), do:  [7, 1, 2, 3, 4, 5, 6] |> Enum.map(&Timex.day_shortname/1)
-  # defp day_names(_), do:  [1, 2, 3, 4, 5, 6, 7] |> Enum.map(&Timex.day_shortname/1)
-
-  # defp week_rows(current_date) do
-  #   first =
-  #     current_date
-  #     |> Timex.beginning_of_month()
-  #     |> Timex.beginning_of_week(@week_start_at)
-
-  #   last =
-  #     current_date
-  #     |> Timex.end_of_month()
-  #     |> Timex.end_of_week(@week_start_at)
-
-  #     Timex.Interval.new(from: first, until: last)
-  #   |> Enum.map(& &1)
-  #   |> Enum.chunk_every(7)
-  # end
+  defp other_month?(assigns) do
+    Map.take(assigns.day, [:year, :month]) != Map.take(assigns.current_date, [:year, :month])
+  end
 end
